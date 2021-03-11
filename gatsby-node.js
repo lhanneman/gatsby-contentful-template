0e0bb0d1a1de7a03fba7904f56110f3e079a65d8
Lloyd Hanneman <lloyd_h2@hotmail.com>
2021-03-10 21:25:07 -0600

    using gatsby-node to create pages based on contentful portfolio data

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        portfolio: allContentfulPortfolio {
+          nodes {
+            slug
+          }
+        }
+      }
+    `).then(({ errors, data }) => {
+      if (errors) {
+        reject(errors)
+      }
+
+      if (data && data.portfolio) {
+        const component = path.resolve("./src/templates/portfolio-item.js")
+        data.portfolio.nodes.map(({ slug }) => {
+          createPage({
+            path: `/${slug}`,
+            component,
+            context: { slug },
+          })
+        })
+      }
+
+      resolve()
+    })
+  })
+}
